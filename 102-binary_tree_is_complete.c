4b50e296c8c7a7af4e8b05440bede19ddd4cc77e
noahmuler <noahtaddese@gmail.com>
2022-11-17 13:27:02 -0800

    Missing Files

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+levelorder_queue_t *create_node(binary_tree_t *node);
+void free_queue(levelorder_queue_t *head);
+void push(binary_tree_t *node, levelorder_queue_t *head,
+		levelorder_queue_t **tail);
+void pop(levelorder_queue_t **head);
+int binary_tree_is_complete(const binary_tree_t *tree);
+
 /**
  * create_node - Creates a new levelorder_queue_t node.
  * @node: The binary tree node for the new node to contain.
